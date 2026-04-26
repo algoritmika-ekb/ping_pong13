b0568fea1dd81e4aa5f1eb6c45c705e55aba3403
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-26 15:10:57 +0500

    Update PING.py13.py

--- PING.py13.py
+++ PING.py13.py
@@ -1,73 +1,75 @@
 from pygame import *
 
+# класс-родитель для спрайтов
 class GameSprite(sprite.Sprite):
-  def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-      sprite.Sprite.__init__(self)
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-      self.image = transform.scale(image.load(player_image), (size_x, size_y))
-      self.speed = player_speed
-      self.rect = self.image.get_rect()
-      self.rect.x = player_x
-      self.rect.y = player_y
-  def reset(self):
-      window.blit(self.image, (self.rect.x, self.rect.y))
-
-
+# класс ракетки
 class Player(GameSprite):
-  def update(self):
-      keys = key.get_pressed()
-      if keys[K_UP] and self.rect.x > 5:
-          self.rect.x -= self.speed
-      if keys[K_DOWN] and self.rect.x < win_width - 80:
-          self.rect.x += self.speed
-  def fire(self):
-      bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-      bullets.add(bullet)
+    def update_r(self):
+        keys = key.get_pressed()
 
-  def update(self):
-      keys = key.get_pressed()
-      if keys[K_w] and self.rect.x > 5:
-          self.rect.x -= self.speed
-      if keys[K_s] and self.rect.x < win_width - 80:
-          self.rect.x += self.speed
-  def fire(self):
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
 
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+
+
+# игровая сцена
 back = (200, 255, 255)
-window = display.set_mode((600, 500))
-window.fill(back)    
+win_width = 600
+win_height = 500
 
+window = display.set_mode((win_width, win_height))
+display.set_caption('Пинг-понг')
+window.fill(back)
+
+# флаги игры
 game = True
-finish = False
-clock = clock.Clock()
-Fps = 60
+clock = time.Clock()
+FPS = 60
 
+# создание ракеток и мяча
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 275, 225, 4, 50, 50)
 
-raсket1 = player('racket.png',  30, 200, 4, 50, 150)
-raсket2 = player('racket.png',  520, 200, 4, 50, 150)
-raсket3 = player('tenis_ball.png',  200, 200, 4, 50, 50)
-
-font.init()
-font = font.Font(None, 35)
-lose 1 = font.render("PLAYER 1 LOSE!",  True, (180,0,0))
-lose 2 = font.render("PLAYER 2 LOSE!",  True, (180,0,0))
-
-speed_x = 3
-speed_y = 3
-
+# игровой цикл
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if finish != True
-            window.fill(back)
-            racket1 = update_1()
-            raсket2 = update_2()
-            ball.rect.x += speed_x
-            ball.rect.y += speed_y
 
-            racket1.reset()
-            racket2.reset()
-            ball.reset()
-        display.update()
-        clock.tick(FPS)
+    window.fill(back)
+
+    racket1.update_l()
+    racket2.update_r()
+
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+
+    display.update()
+    clock.tick(FPS)
